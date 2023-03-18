217b733c3fd5d434b664a08ce3a15b0255e9c92f
Roko Mihalić <roko.mihalic@leapbit.com>
2023-03-19 00:56:17 +0100

    get users

--- backend/UserManagement.js
+++ backend/UserManagement.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-unused-vars */
 /* eslint-disable consistent-return */
 /* eslint-disable no-console */
@@ -9,8 +10,6 @@ const cors = require('cors');
 const Logger = require('./Activity');
 const Stats = require('./Stats');
 const MFA = require('./MFA');
-
-const allowedOrigins = ['213.202.75.60', '213.202.75.60'];
 
 const ac = new AccessControl();
 
@@ -30,17 +29,11 @@ ac.grant('admin')
 class UserManagement {
   constructor(hash) {
     this.app = express();
-    this.app.use(cors({
-      origin(origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-          const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-          return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-      },
+    this.app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      next();
+    });
 
-    }));
     this.users = [
       { username: 'user3', password: 'password3', role: 'base' },
       { username: 'user1', password: 'password1', role: 'base' },
@@ -50,8 +43,7 @@ class UserManagement {
     this.secretKey = hash;
     this.authMiddleware = this.authMiddleware.bind(this);
     this.login = this.login.bind(this);
-    this.getProfile = this.getProfile.bind(this);
-    this.updateProfile = this.updateProfile.bind(this);
+    this.getUsers = this.getUsers.bind(this);
     this.deleteProfile = this.deleteProfile.bind(this);
     this.start = this.start.bind(this);
     this.verify = this.verify.bind(this);
@@ -90,7 +82,7 @@ class UserManagement {
       }
 
       const token = jwt.sign({ username, role: (userData.data.admin) ? 'admin' : 'base' }, me.secretKey);
-      return res.status(200).json({ error: true, token });
+      return res.status(200).json({ error: false, token });
     }
     return res.status(401).json({ error: true, message: 'Unsuccessfull MultiFactor Authentication' });
   }
@@ -167,61 +159,56 @@ class UserManagement {
     return res.status(400).json({ error: true, data: {}, notice: 'NO ACCESS' });
   }
 
-  getProfile(req, res) {
+  async getUsers(req, res) {
+    const me = this;
     const { username } = req.params;
     const { role } = req.user;
-
     if (ac.can(role).readAny('profile').granted) {
-      const user = this.users.find((u) => u.username === username);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+      const users = [];
+      try {
+        const dataDb = await global.pgdb.query('SELECT * FROM public.users;');
+        if (dataDb) {
+          const data = dataDb.rows;
+          for (const user of data) {
+            const userObj = {
+              username: user.username,
+              riskFactor: user.risk_factor,
+              location: user.location,
+              role: (user.admin) ? 'admin' : 'base',
+            };
+            users.push(userObj);
+          }
+          console.log('gotten user data from DB', data);
+          return res.status(200).json({ error: false, data: { users } });
+        }
+        throw new Error();
+      } catch (e) {
+        console.log(e);
+        return { error: true, data: {}, notice: 'Internal error' };
       }
-      return res.status(200).json({ username: user.username, role: user.role });
     }
-
-    if (ac.can(role).readOwn('profile').granted && username === req.user.username) {
-      return res.status(200).json({ username, role });
-    }
-
     res.status(403).json({ message: 'Forbidden' });
   }
 
-  updateProfile(req, res) {
-    const { username } = req.params;
-    const { role } = req.user;
-
-    if (ac.can(role).updateAny('profile').granted) {
-      const user = this.users.find((u) => u.username === username);
-      if (!user) {
-        return res.status(404).json({ message: 'User not found' });
-      }
-
-      const { newUsername } = req.body;
-      user.username = newUsername;
-      return res.status(200).json({ message: 'Profile updated successfully' });
-    }
-
-    if (ac.can(role).updateOwn('profile').granted && username === req.user.username) {
-      const { newUsername } = req.body;
-      this.users.find((u) => u.username === username).username = newUsername;
-      return res.status(200).json({ message: 'Profile updated successfully' });
-    }
-
-    res.status(403).json({ message: 'Forbidden' });
-  }
-
-  deleteProfile(req, res) {
+  async deleteProfile(req, res) {
     const { username } = req.params;
     const { role } = req.user;
 
     if (ac.can(role).deleteAny('profile').granted) {
-      const userIndex = this.users.findIndex((u) => u.username === username);
-      if (userIndex === -1) {
-        return res.status(404).json({ message: 'User not found' });
+      const userData = await MFA.getUserData(username);
+      if (userData.error) {
+        return res.status(401).json({ error: true, message: 'User not found' });
       }
-
-      this.users.splice(userIndex, 1);
-      return res.status(200).json({ message: 'Profile deleted successfully' });
+      try {
+        await global.pgdb.query(
+          'DELETE FROM public.users WHERE username = $1;',
+          [username],
+        );
+      } catch (e) {
+        console.log(e);
+        return res.status(404).json({ error: true, data: {}, notice: 'Internal error' });
+      }
+      return res.status(200).json({ error: false, message: 'Profile deleted successfully' });
     }
 
     if (ac.can(role).deleteOwn('profile').granted && username === req.user.username) {
@@ -242,10 +229,9 @@ class UserManagement {
     this.app.post('/login', this.login);
     this.app.post('/verify', this.verify);
     this.app.post('/createProfile', this.authMiddleware, this.createProfile);
-    this.app.get('/users/:username', this.authMiddleware, this.getProfile);
+    this.app.get('/users', this.authMiddleware, this.getUsers);
     this.app.get('/simulateUrl/:url', this.authMiddleware, this.simulateUrl);
     this.app.get('/getStats', this.authMiddleware, this.getStats);
-    this.app.put('/users/:username', this.authMiddleware, this.updateProfile);
     this.app.post('/delete', this.authMiddleware, this.deleteProfile);
 
     this.app.listen(3000, () => {
